4cf94cb1732795d31087ca1834334c2014e4f311
sephdo <joe@electinc.com>
2018-10-28 13:28:58 -0500

    Added windows to terraced floors

--- cityblock/app.py
+++ cityblock/app.py
@@ -70,10 +70,12 @@ class HouseGenerator:
             WINDOW_MARGIN=RandList([0,1,1,2,2,3,3,]),
             WINDOW_BORDERS=RandList(['all','vertical','horizontal','bottom']),
             WINDOW_HAS_BORDERS=RandList([True,True,False]),
+            WINDOW_DELTA=RandList([1,3]), #Used if window_count is reduced and a large central window is added
             
             ROOF_PITCH=RandRange(.1,.5,'float'),
             ROOF_AWNING=RandRange(0,10,'even'),
             ROOF_TYPE=RandList([SimpleRoof,FlatRoof,NullRoof,StaggeredRoof,FpRoof]),
+            #ROOF_TYPE=RandList([FpRoof]),
             ROOF_DIRECTION=RandBool(),
             ROOF_LEVELS=RandRange(2,4),
             ROOF_STEP_LENGTH=RandRange(3,5),
@@ -101,6 +103,45 @@ class HouseGenerator:
     def makeHouses(self):
         for i in range(self.neighborhood.HOUSE_COUNT):
             yield self.makeHouse(twin=True if random.random() >= self.neighborhood.TWIN_THRESHOLD else False)
+    def addWindows(self,wall,data,colors,borderless=True,protruding_roof=None):
+        # TODO: adjust window height when roof overlaps bottom of windows
+        window_count = int((wall.width-2*data.WINDOW_MARGIN)/(data.WINDOW_WIDTH+data.WINDOW_MARGIN))
+        window_height = data.WINDOW_HEIGHT
+        wall_height = wall.height
+        ## ALT EFFECT: borderless #1
+        if borderless:
+            vertical = True if 'left' in data.WINDOW_BORDERS else False
+            if data.WINDOW_MARGIN == 0 and wall.width > (window_count*data.WINDOW_WIDTH):
+                if window_count % 2 == 0:
+                    window_count -= data.WINDOW_DELTA
+                expand = window_count >> 1
+                extra = wall.width-(window_count*data.WINDOW_WIDTH)
+            else:
+                expand = window_count-1
+                extra = 0
+        windows = []
+        borders = data.WINDOW_BORDERS.copy()
+        for j in range(window_count):
+            fill = cm.tupwoggle(colors.LIGHT_WINDOW,10) if random.random()>self.neighborhood.WINDOW_LIGHT_PCT else colors.WINDOW
+            
+            #ALT EFFECT borderless #2
+            if borderless:
+                if vertical and data.WINDOW_MARGIN == 0:
+                    if j > 0:
+                        borders.discard('left')
+                if data.WINDOW_MARGIN == 0 and j == expand:
+                    window = Window(w=data.WINDOW_WIDTH+extra,h=window_height,stroke=colors.WINDOWSILL,borders=borders,fill=fill)
+                else:
+                    window = Window(w=data.WINDOW_WIDTH,h=window_height,stroke=colors.WINDOWSILL,borders=borders,fill=fill)
+            else:
+                window = Window(w=data.WINDOW_WIDTH,h=window_height,stroke=colors.WINDOWSILL,borders=borders,fill=fill)
+            windows.append((window,data.WINDOW_MARGIN if j>0 else 0,0))
+        if len(windows)>0:
+            c_windows = Canvas.fromStack(windows,vertical=False)
+            floor = Canvas.fromSprites([(wall,0,0),(c_windows,int((wall.width-c_windows.width)/2),int((wall.height-wall_height)/2+(wall.height-c_windows.height)/2))])
+        else:
+            floor = wall
+        return floor
     def makeHouse(self,twin=False):
         # ALT EFFECTS - not sure if these will make it into production code
         terrace = True
@@ -115,28 +156,19 @@ class HouseGenerator:
         else:
             data = SimpleNamespace(**{key:value.rand() for key,value in self.ranges.items()})
             colors = SimpleNamespace(**{key:value.rand() for key,value in self.colors.items()})
-            ## Exclusionary fields
-            # note - won't have any effect, as wall color has already been woggled
-            """if not self.colors['ROOF'].frozen:
-                while colors.ROOF == colors.WALL:
-                    colors.ROOF = self.colors['ROOF'].rand()"""
             ## Secondary fields
             data.WALL_BORDERS = Rect.defineSills(data.WALL_BORDERS if data.WALL_HAS_BORDERS else None)
-            #data.WINDOW_HAS_BORDERS = data.WINDOW_HAS_BORDERS if data.WINDOW_WIDTH > 5 else False
             data.WINDOW_BORDERS = Window.defineSills(data.WINDOW_BORDERS if data.WINDOW_HAS_BORDERS else None)
             data.WINDOW_HEIGHT = int(data.FLOOR_HEIGHT*data.WINDOW_FLOOR_RATIO)
-            data.WINDOW_COUNT = int((data.FLOOR_WIDTH-2*data.WINDOW_MARGIN)/(data.WINDOW_WIDTH+data.WINDOW_MARGIN))
             data.ROOF_AWNING = data.ROOF_AWNING if data.ROOF_TYPE.HAS_AWNINGS else 0
             data.ROOF_WIDTH = data.FLOOR_WIDTH+data.ROOF_AWNING
             data.ROOF_HEIGHT = int(data.ROOF_WIDTH*data.ROOF_PITCH)
             
             colors.ROOF_STROKE = colors.ROOF_STROKE if data.ROOF_HAS_SURFACE else colors.ROOF
             colors.WALL_STROKE = cm.tupwoggle(colors.WALL,45)
-            ## Tertiary fields - depend on prev (?)
+
+            ## Static fields
             data.ALLEY_WIDTH = 5
-            """data.ALLEY_WIDTH += (data.ROOF_AWNING >> 2)
-            if self.prev:
-                data.ALLEY_WIDTH += self.prev[0].ROOF_AWNING >> 2"""
             
         ## Create sprites
         roof = data.ROOF_TYPE(
@@ -154,8 +186,9 @@ class HouseGenerator:
 
         ## ALT EFFECT: terrace - works well with short roofs
         if terrace:
-            if data.FLOOR_COUNT < 3 and not data.ROOF_TYPE in [NullRoof] and data.ROOF_HEIGHT < data.FLOOR_HEIGHT:
+            if data.FLOOR_COUNT < 3 and data.ROOF_TYPE in [FpRoof,FlatRoof,StaggeredRoof]:
                 roof_bg = Rect(w=int(3*data.FLOOR_WIDTH/4),h=data.FLOOR_HEIGHT,fill=colors.WALL)
+                roof_bg = self.addWindows(roof_bg,data,colors,borderless=borderless,protruding_roof=roof)
                 if roof.width > roof_bg.width:
                     roof = Canvas.fromSprites([(roof_bg,int((roof.width-roof_bg.width)/2),0),(roof,0,0)])
                 else:
@@ -172,17 +205,7 @@ class HouseGenerator:
                     )
                 roof = Canvas.fromStack([roof,roof2])
 
-        ## ALT EFFECT: borderless #1
-        ## When WINDOW_MARGIN == 0, windows should take up entire width of floor
-        if borderless:
-            vertical = True if 'left' in data.WINDOW_BORDERS else False
-            if data.WINDOW_MARGIN == 0 and data.FLOOR_WIDTH > (data.WINDOW_COUNT*data.WINDOW_WIDTH):
-                if data.WINDOW_COUNT % 2 == 0:
-                    data.WINDOW_COUNT -= random.choice([1,3])
-                expand = data.WINDOW_COUNT >> 1
-                extra = data.FLOOR_WIDTH-(data.WINDOW_COUNT*data.WINDOW_WIDTH)
-            else:
-                expand = data.WINDOW_COUNT
+        
 
         floors = []
         for i in range(data.FLOOR_COUNT):
@@ -192,38 +215,8 @@ class HouseGenerator:
             if wallstroke:
                 borders = data.WALL_BORDERS.difference({'top'}) if i+1 == data.FLOOR_COUNT else data.WALL_BORDERS
                 wall = Rect(w=data.FLOOR_WIDTH,h=data.FLOOR_HEIGHT,fill=colors.WALL,stroke=colors.WALL_STROKE,borders=borders)
-
-            windows = []
-            for j in range(data.WINDOW_COUNT):
-                fill = cm.tupwoggle(colors.LIGHT_WINDOW,10) if random.random()>self.neighborhood.WINDOW_LIGHT_PCT else colors.WINDOW
-                
-                window = Window(w=data.WINDOW_WIDTH,h=data.WINDOW_HEIGHT,stroke=colors.WINDOWSILL,borders=data.WINDOW_BORDERS,fill=fill)
-
-                #ALT EFFECT borderless #2
-                if borderless:
-                    if vertical and data.WINDOW_MARGIN == 0:
-                        if j < expand:
-                            data.WINDOW_BORDERS.add('left')
-                            data.WINDOW_BORDERS.discard('right')
-                        elif j == expand:
-                            data.WINDOW_BORDERS.add('left')
-                            data.WINDOW_BORDERS.add('right')
-                        else:
-                            data.WINDOW_BORDERS.discard('left')
-                            data.WINDOW_BORDERS.add('right')
-                    if data.WINDOW_MARGIN == 0 and j == expand:
-                        window = Window(w=data.WINDOW_WIDTH+extra,h=data.WINDOW_HEIGHT,stroke=colors.WINDOWSILL,borders=data.WINDOW_BORDERS,fill=fill)
-                    else:
-                        window = Window(w=data.WINDOW_WIDTH,h=data.WINDOW_HEIGHT,stroke=colors.WINDOWSILL,borders=data.WINDOW_BORDERS,fill=fill)
-                windows.append((window,data.WINDOW_MARGIN if j>0 else 0,0))
-            if len(windows)>0:
-                c_windows = Canvas.fromStack(windows,vertical=False)
-                floor = Canvas.fromSprites([(wall,0,0),(c_windows,int((wall.width-c_windows.width)/2),int((wall.height-c_windows.height)/2))])
-            else:
-                floor = wall
-            floors.append(floor)
+            floors.append(self.addWindows(wall,data,colors,borderless=borderless))
         c_floors = Canvas.fromStack(floors)
         c = Canvas.fromStack([c_floors,roof])
-        #return c,data.ALLEY_WIDTH-int(data.ROOF_AWNING/2),0
         self.prev = (data,colors)
         return c, data.ALLEY_WIDTH, 0
